01f87560a0a36cea3058fcc9dc3cb2bc69e6701d
fab <fabienbeaudimi@hotmail.fr>
2012-06-21 04:11:10 +0200

    Signed-off-by: fab <fabienbeaudimi@hotmail.fr>

---
+++ app/template/GCsystemDev.tpl
@@ -0,0 +1,35 @@
+<style>
+	#GCsysytem_dev{
+		position: fixed;
+		background-color: #F7F7F7;
+		background-image: -moz-linear-gradient(-90deg, #E4E4E4, white);
+		background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#E4E4E4), to(white));
+		bottom: 0;
+		left: 0;
+		margin: 0;
+		padding: 5px;
+		z-index: 6000000;
+		width: 100%;
+		border-top: 1px solid 
+		#BBB;
+		font: 16px Verdana, Arial, sans-serif;
+		text-align: left;
+		color: 
+		#2F2F2F;
+	}
+	
+	#GCsysytem_dev_logo{
+		display: inline-block;
+	}
+	
+	#GCsysytem_dev_content{
+		position: relative; 
+		top: -6px; 
+		display: inline-block;
+	}
+	
+</style>
+<div id="GCsysytem_dev">
+	<div id="GCsysytem_dev_logo"><img src="{IMG_PATH}logo.png" style="width: 25px" /></div>
+	<div id="GCsysytem_dev_content">{text}</div>
+</div>
--- system/class/appDev.class.php
+++ system/class/appDev.class.php
@@ -11,25 +11,14 @@
 	class appDev{
 		
 		public  function __construct(){
-			echo '<div id="dev" style="position: fixed;
-				background-color: #F7F7F7;
-				background-image: -moz-linear-gradient(-90deg, #E4E4E4, white);
-				background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#E4E4E4), to(white));
-				bottom: 0;
-				left: 0;
-				margin: 0;
-				padding: 5px;
-				z-index: 6000000;
-				width: 100%;
-				border-top: 1px solid 
-				#BBB;
-				font: 16px Verdana, Arial, sans-serif;
-				text-align: left;
-				color: 
-				#2F2F2F;">
-					<div id="dev_logo" style=" display: inline-block;"><img src="'.IMG_PATH.'logo.png" style="width: 25px" /></div>
-					<div id="dev_text" style="position: relative; top: -6px; display: inline-block;">interface de développement en cours de création</div>
-				</div>';
+			$tpl = new templateGC('GCsystemDev', 'GCsystemDev', 0);
+			
+			$tpl->assign(array(
+				'text'=>"interface de développement en cours de création",
+				'IMG_PATH'=>IMG_PATH
+			));
+				
+			$tpl->show();
 		}
 		
 		public  function __desctuct(){
