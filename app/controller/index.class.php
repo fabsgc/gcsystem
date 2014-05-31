23c99e22a42d333ea542d7cd877d1b037279c358
fabsgc <fabienbeaudimi@hotmail.fr>
2014-05-31 20:13:49 +0200

    petite correction du contrôleur index

--- app/controller/index.class.php
+++ app/controller/index.class.php
@@ -7,12 +7,6 @@
 		}
 
 		public function actionDefault(){
-			$this->event->add(new system\event('myEvent', $this, array()));
-			$this->event->add(new system\event('myEvent2', $this, array()));
-			$this->event->dispatch();
-
-			//print_r($this->event->getResult());
-
 			$t = new system\template(GCSYSTEM_PATH.'GCsystem', 'GCsystem', 0, $this->lang);
 			$t->show();
 		}
