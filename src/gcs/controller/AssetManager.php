bb50f7dde5f6eebf85bc9f527d9614e5faa71c13
fabsgc <fabienbeaudimi@hotmail.fr>
2016-07-07 02:02:07 +0200

    asset manager

--- src/gcs/controller/AssetManager.php
+++ src/gcs/controller/AssetManager.php
@@ -7,18 +7,12 @@
 	use System\Response\Response;
 
 	class AssetManager extends Controller {
-		public function init() {
-			if (Config::config()['user']['debug']['environment'] != 'development') {
-				Response::instance()->status(404);
-			}
-		}
-
 		public function actionDefault() {
 			if ($_GET['type'] == 'js' || $_GET['type'] == 'css') {
 				Response::instance()->contentType("text/" . $_GET['type']);
 				Response::instance()->header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 2592000));
 
-				$cache = new Cache(html_entity_decode($_GET['id'] . '.' . $_GET['type']), 0);
+				$cache = new Cache(html_entity_decode($_GET['id'] . '.' . $_GET['type']), Config::config()['user']['output']['asset']['cache']);
 
 				return $cache->getCache();
 			}
